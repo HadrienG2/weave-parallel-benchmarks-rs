5c53aadfa9892c49584084d565baa26aa10becc5
Hadrien G <knights_of_ni@gmx.com>
2019-12-09 23:37:56 +0100

    Not, that was not the faithful DFS version yet. It's actually even worse :(

--- src/bin/dfs.rs
+++ src/bin/dfs.rs
@@ -1,24 +1,41 @@
 use rayon::prelude::*;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 fn dfs(depth: usize, breadth: usize) -> usize {
     if depth == 0 {
         return 1;
     }
 
-    // It always start with scheduling "breadth" tasks, as a parallel iterator
-    // since that's the most natural Rayon tool for that job
-    let res_iter = (0..breadth).into_par_iter()
-                               .map(|_| dfs(depth-1, breadth));
-
     if cfg!(feature = "idiomatic") {
-      // Idiomatic mode, do what a Rayon user normally would and sum in the same
-      // Rayon transaction for optimal performance
-      res_iter.sum()
+      // Idiomatic mode, do what a Rayon user normally would and carry out all
+      // the work in a single map-reduce transaction for optimal performance
+      (0..breadth).into_par_iter()
+                  .map(|_| dfs(depth-1, breadth))
+                  .sum()
     } else {
-      // Non-idiomatic mode, strictly imitate the Weave version and collect the
-      // data into a heap-allocated Vec, followed by a sequential sum
-      let vec = res_iter.collect::<Vec<_>>();
-      vec.into_iter().sum()
+      // Non-idiomatic mode, strictly imitate the Weave version by...
+      //
+      // 1. Preallocating a vector of outputs instead of collecting an iterator
+      // 2. Explicitly spawning one task for each of the "breadth" iterations
+      // 3. Performing a serial sum at the end
+      //
+      // You can also experiment with ligher shades of non-idiomatic code, by
+      // e.g. using a parallel Rayon iterator instead of an explicit loop, but
+      // still collecting the outputs in a vector and ending with a serial sum.
+      //
+      let vec = (0..breadth).map(|_| AtomicUsize::new(0))
+                            .collect::<Vec<_>>();
+
+      rayon::scope(|s|{
+        for target in vec.iter() {
+          s.spawn(move |_| target.store(dfs(depth-1, breadth),
+                                        Ordering::Relaxed));
+        }
+      });
+
+      vec.into_iter()
+         .map(|a| a.load(Ordering::Relaxed))
+         .sum()
     }
 }
 
